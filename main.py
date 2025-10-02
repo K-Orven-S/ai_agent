625748802bad3da851a72862b8b440c0f6fb0d38
K-Orven-S <kimorvens@gmail.com>
2025-10-01 21:10:48 -0400

    Laid out foundation for prompt and response

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+user_prompt = sys.argv[1]
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model='gemini-2.0-flash-001', contents=messages
 )
 
+
+
 def main():
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(response.text)
+    
+    if len(sys.argv) == 1:
+        print("Error: Please provide a prompt")
+        sys.exit(1)
+    elif "--verbose" in sys.argv:  
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}") 
+        print(response.text)
+    else:
+        print(response.text)
+        print(messages)
+        
 
 
 if __name__ == "__main__":
